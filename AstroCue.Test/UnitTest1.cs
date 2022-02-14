ec08ce5b3b25b5a56081b8ac4546244e4a050758
Reece Mercer <reecemercer981@gmail.com>
2022-02-14 01:07:09 +0000

    Added test project to solution

---
+++ AstroCue.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+namespace AstroCue.Test
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
