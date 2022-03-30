e7276559ecdf94b358fe850b65c5a9d64e5277b5
Reece Mercer <reecemercer981@gmail.com>
2022-03-31 00:19:28 +0100

    #39 MST and Julian Day equations

---
+++ AstroCue.Server/Astronomy/CoordinateTransformations.cs
@@ -0,0 +1,94 @@
+﻿namespace AstroCue.Server.Astronomy
+{
+    using System;
+    using Entities;
+    using Entities.Owned;
+
+    /// <summary>
+    /// Class to carry out various coordinate transformation operations
+    /// </summary>
+    public static class CoordinateTransformations
+    {
+        public static AltAz EquatorialToHorizontal(AstronomicalObject obj, DateTime date, float longitude, float latitude)
+        {
+            if (date.Kind != DateTimeKind.Utc)
+            {
+                throw new ArgumentException("Date needed in UTC", nameof(date));
+            }
+
+
+        }
+
+        /// <summary>
+        /// Calculates the mean sidereal time at Greenwich for any instant UTC
+        /// https://en.wikipedia.org/wiki/Sidereal_time
+        /// </summary>
+        /// <param name="instant">A <see cref="DateTime"/> instance in UTC</param>
+        /// <returns>Mean sidereal time at Greenwich for the instant given</returns>
+        public static double MeanSiderealTimeAtInstant(DateTime instant)
+        {
+            if (instant.Kind != DateTimeKind.Utc)
+            {
+                throw new ArgumentException("Date needed in UTC", nameof(instant));
+            }
+
+            double jd = DateToJulianDay(instant);
+            double t = (jd - 2451545.0) / 36525;
+            
+            double mst = 280.46061837 + 360.98564736629 * (jd - 2451545.0) + 0.000387933 * (t * t) - t * t * t / 3871000;
+
+            // bring in range 0-360
+            if (mst < 360)
+            {
+                while (mst < 0)
+                {
+                    mst += 360;
+                }
+            }
+
+            if (mst > 360)
+            {
+                while (mst > 360)
+                {
+                    mst -= 360;
+                }
+            }
+
+            return mst;
+        }
+
+        /// <summary>
+        /// Calculate the Julian Day of a given date
+        /// https://en.wikipedia.org/wiki/Julian_day
+        /// 
+        /// "The Julian Day number or, more simply, the Julian Day (*) (JD) is a continuous
+        /// count of days and fractions thereof from the beginning of the year -4712. By tradition,
+        /// the Julian Day begins at Greenwich mean noon, that is, at 12h Universal Time."
+        /// (Meeus, 1998, p. 59-66)
+        /// </summary>
+        /// <param name="instant">A <see cref="DateTime"/> instance in UTC</param>
+        /// <returns>The date's Julian Day</returns>
+        public static double DateToJulianDay(DateTime instant)
+        {
+            if (instant.Kind != DateTimeKind.Utc)
+            {
+                throw new ArgumentException("Date needed in UTC", nameof(instant));
+            }
+
+            int year = instant.Year;
+            int month = instant.Month;
+            double dayFrac = instant.Hour / 24.0 + instant.Minute / 1440.0 + instant.Day;
+
+            if (!(month > 2))
+            {
+                year -= 1;
+                month += 12;
+            }
+
+            int a = (int)Math.Floor(year / 100.0);
+            int b = 2 - a + (int)Math.Floor(a / 4.0);
+             
+            return Math.Floor(365.25 * (year + 4716)) + Math.Floor(30.6001 * (month + 1)) + dayFrac + b - 1524.5;
+        } 
+    }
+}
---
+++ AstroCue.Test/Astronomy/CoordinateTransformationsTests.cs
@@ -0,0 +1,54 @@
+﻿namespace AstroCue.Test.Astronomy
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+    using FluentAssertions;
+    using FluentAssertions.Extensions;
+    using Server.Astronomy;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    /// Tests targeting <see cref="CoordinateTransformations"/>
+    /// </summary>
+    [TestClass]
+    [ExcludeFromCodeCoverage]
+    public class CoordinateTransformationsTests
+    {
+        /// <summary>
+        /// Tests the <see cref="CoordinateTransformations.DateToJulianDay"/> method.
+        /// Tests cases (Meeus, 1998) 
+        /// </summary>
+        [TestMethod]
+        public void DateToJulianDayTest()
+        {
+            // Arrange
+            DateTime test1 = DateTime.Parse("4 October 1957, 7:28 pm").AsUtc();
+            DateTime test2 = DateTime.Parse("10 April 1987, 00:00 am").AsUtc();
+
+            // Act
+            double jd1 = CoordinateTransformations.DateToJulianDay(test1);
+            double jd2 = CoordinateTransformations.DateToJulianDay(test2);
+
+            // Assert
+            Math.Round(jd1, 2).Should().Be(2436116.31);
+            Math.Round(jd2, 1).Should().Be(2446895.5);
+            Assert.ThrowsException<ArgumentException>(() =>
+                CoordinateTransformations.DateToJulianDay(DateTime.Now.AsLocal()));
+        }
+
+        [TestMethod]
+        public void MeanSiderealTimeAtInstantTest()
+        {
+            // Arrange
+            DateTime test1 = DateTime.Parse("10 April 1987, 7:21 pm").AsUtc();
+
+            // Act
+            double mst1 = CoordinateTransformations.MeanSiderealTimeAtInstant(test1);
+
+            // Assert
+            Math.Round(mst1, 7).Should().Be(128.7378732);
+            Assert.ThrowsException<ArgumentException>(() =>
+                CoordinateTransformations.MeanSiderealTimeAtInstant(DateTime.Now.AsLocal()));
+        }
+    }
+}
